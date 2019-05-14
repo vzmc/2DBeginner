d5aee08ae7f8a35f3d5fd9b7f138c1dad96be0b9
zhangyubing <zyb@aiming-inc.com>
2019-05-14 19:21:37 +0900

    特效追加

--- 2DBeginner/Assets/Scripts/EnemyController.cs
+++ 2DBeginner/Assets/Scripts/EnemyController.cs
@@ -7,6 +7,7 @@ public class EnemyController : MonoBehaviour
     public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
+    public ParticleSystem smokeEffect;
 
     Rigidbody2D rigidbody2D;
     float timer;
@@ -73,5 +74,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2D.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
